a7461cdd7d313fecfb2dc2017d56120f35bdd533
ilkin <huseynlilkin@gmail.com>
2021-09-11 19:18:07 +0200

    #7 - Stateless widget added

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,36 @@
 import 'package:flutter/material.dart';
 
-void main() =>
-    runApp(MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('my first app'),
-          backgroundColor: Colors.red[600],
-          centerTitle: true,
-        ),
-        body: Center(
-          child: Text(
-            "Hello ninjas!",
-            style: TextStyle(
-              fontSize: 20.0,
-              fontWeight: FontWeight.bold,
-              letterSpacing: 2.0,
-              color: Colors.grey[600],
-            ),
+void main() => runApp(MaterialApp(
+      home: Home(),
+    ));
 
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('my first app'),
+        backgroundColor: Colors.red[600],
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Text(
+          "Hello ninjas!",
+          style: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2.0,
+            color: Colors.grey[600],
           ),
         ),
-        floatingActionButton: FloatingActionButton(
-          child: Text('click'),
-          backgroundColor: Colors.green,
-          onPressed: () => {},
-        ),
       ),
-
-    ));
+      floatingActionButton: FloatingActionButton(
+        child: Text('click'),
+        backgroundColor: Colors.red,
+        onPressed: () => {},
+      ),
+    );
+  }
+}
