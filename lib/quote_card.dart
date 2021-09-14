d4c0a13857d0222fcdcfe251c75e0254380d7ece
ilkin <huseynlilkin@gmail.com>
2021-09-14 23:36:05 +0200

    #19 - removing cards by clicking button

--- lib/quote_card.dart
+++ lib/quote_card.dart
@@ -3,8 +3,9 @@ import 'quote.dart';
 
 class QuoteCard extends StatelessWidget {
   final Quote quote;
+  final Function() delete;
 
-  QuoteCard({required this.quote});
+  QuoteCard({required this.quote, required this.delete});
 
   @override
   Widget build(BuildContext context) {
@@ -21,6 +22,11 @@ class QuoteCard extends StatelessWidget {
             Text(
               quote.author,
               style: TextStyle(fontSize: 14.0, color: Colors.grey[800]),
+            ),
+            SizedBox(height: 6.0),
+            TextButton(
+              onPressed: delete,
+              child: Icon(Icons.delete, color: Colors.black),
             )
           ],
         )));
