df60ebc62b208e3f634ab90003d06b0480c36fc2
ilkin <huseynlilkin@gmail.com>
2021-09-20 22:02:36 +0200

    #22 - Starting a new project: World Clocks

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,7 @@
+import 'package:demo2/pages/home.dart';
 import 'package:flutter/material.dart';
-import 'quote.dart';
-import 'quote_card.dart';
 
-void main() => runApp(MaterialApp(home: QuoteList()));
 
-class QuoteList extends StatefulWidget {
-  const QuoteList({Key? key}) : super(key: key);
-
-  @override
-  _QuoteListState createState() => _QuoteListState();
-}
-
-class _QuoteListState extends State<QuoteList> {
-  List<Quote> quotes = [
-    Quote(author: 'Less known letter', text: 'Some random long text just to see how it works'),
-    Quote(author: 'An author known by no one', text: 'Another very meaningful quote'),
-    Quote(author: 'Well known author', text: 'Very useful quote')
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        backgroundColor: Colors.grey[200],
-        appBar: AppBar(
-          title: Text('Some quotes'),
-          centerTitle: true,
-          backgroundColor: Colors.redAccent,
-        ),
-        body: Column(
-          children: quotes.map((quote) => QuoteCard(
-              quote: quote,
-              delete: () {
-                setState(() {
-                  quotes.remove(quote);
-                });
-              }
-          )).toList(),
-        ));
-  }
-}
+void main() => runApp(MaterialApp(
+  home: Home(),
+));
